f3ee1649ff62394e542a658ee27a17a3a548e171
Z4R4N05 <eddypsc@gmail.com>
2022-02-22 21:30:21 -0500

    first setup

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,8 +1,26 @@
-import React from 'react';
+import React, {useState} from 'react';
+import {CopyToClipboard } from 'react-copy-to-clipboard'
 
 function Home(){
+    const [value, setValue] = useState ('')
+    const [copied, setCopy] = useState (false)
     return (<>
-        <h1>Home</h1>
+        <h1>Splat</h1>
+        <div>
+          <input value={value}
+            onChange={({target}) => {
+                setValue(target.value)
+                setCopy(false)
+            } } />
+  
+          
+          <CopyToClipboard text={value}
+            onCopy={() => setCopy(true)}>
+            <button>Copy to clipboard with button</button>
+          </CopyToClipboard>
+  
+          {copied ? <span style={{color: 'red'}}>Copied.</span> : null}
+        </div>
     </>)
 }
 
